74accef94172de5f89cf668e2e832bd895483811
yhybulider <1065205068@qq.com>
2021-03-22 16:28:07 +0800

    提交导航区的引用，使用elui的设计

--- src/components/admin/UserList.vue
+++ src/components/admin/UserList.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>用户列表</div>
+  <div>
+    用户列表
+    <!-- 导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+<!-- 用户列表的主体 -->
+    <el-card>
+        <!-- 搜索区域 -->
+        
+
+    </el-card>
+  </div>
 </template>
 <script>
-export default {
-        
-}
+export default {};
 </script>
 
 <style lang = "less" scoped>
-
+.el-breadcrumb{
+    margin-bottom: 15px;
+    font-size: 12px;
+}
 </style>
