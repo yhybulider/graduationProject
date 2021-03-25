4d0cc3a2554f49c1e87fc6c2861524b7d523f71d
yhybulider <1065205068@qq.com>
2021-03-25 15:52:13 +0800

    添加用户的对话框前端展示

--- src/components/admin/UserList.vue
+++ src/components/admin/UserList.vue
@@ -13,13 +13,19 @@
       <el-row :gutter="25">
         <el-col :span="10">
           <!-- 搜索添加 搜索的功能通过将query来重新提交表单搜索-->
-          <el-input placeholder="请输入搜索内容" v-model="queryInfo.query" clearable @clear="getUserList" >
-            <el-button slot="append" icon="el-icon-search" @click="getUserList"> </el-button>
+          <el-input
+            placeholder="请输入搜索内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getUserList"
+          >
+            <el-button slot="append" icon="el-icon-search" @click="getUserList">
+            </el-button>
           </el-input>
         </el-col>
         <!-- 同行的按钮 -->
         <el-col :span="4">
-          <el-button type="primary" @click="addDialogVisiable = true">
+          <el-button type="primary" @click="addDialogVisible = true">
             添加用户
           </el-button>
         </el-col>
@@ -27,45 +33,90 @@
 
       <!-- 用户列表的绘制 -->
       <el-table :data="userlist" border stripe>
+        <el-table-column label="用户" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="密码" prop="password"></el-table-column>
+        <el-table-column label="角色" prop="role"></el-table-column>
+        <el-table-column label="状态" prop="state">
+          <!-- 作用域插槽 -->
+          <template slot-scope="scope">
+            <!-- scope.row每一行封存的数据 -->
+            <el-switch
+              v-model="scope.row.state"
+              @change="userStateChanged(scope.row)"
+            ></el-switch>
+          </template>
+        </el-table-column>
 
-          <el-table-column label="用户" prop="username"></el-table-column>
-          <el-table-column label="邮箱" prop="email"></el-table-column>
-          <el-table-column label="密码" prop="password"></el-table-column>
-          <el-table-column label="角色" prop="role"></el-table-column>
-          <el-table-column label="状态" prop="state">
-
-              <!-- 作用域插槽 -->
-              <template slot-scope="scope">
-                  <!-- scope.row每一行封存的数据 -->
-                  <el-switch v-model="scope.row.state" @change="userStateChanged(scope.row)"></el-switch>
-              </template>
-          </el-table-column>
-
-          <el-table-column label="操作">
-              <template>
-                  <!-- 修改 -->
-                  <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-                  <!-- 删除 -->
-                  <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-                  <!-- 权限 -->
-                  <el-tooltip effect="dark" content="分配权限" placement="top-start" :enterable="false">
-                      <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
-                  </el-tooltip>
-              </template>
-          </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <!-- 修改 -->
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <!-- 删除 -->
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+            ></el-button>
+            <!-- 权限 -->
+            <el-tooltip
+              effect="dark"
+              content="分配权限"
+              placement="top-start"
+              :enterable="false"
+            >
+              <el-button
+                type="warning"
+                icon="el-icon-setting"
+                size="mini"
+              ></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
 
       <!-- 分页操作 -->
       <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="queryInfo.pageNum"
-      :page-sizes="[1, 2, 5, 100]"
-      :page-size="queryInfo.pageNum"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pageNum"
+        :page-sizes="[1, 2, 5, 100]"
+        :page-size="queryInfo.pageNum"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
+
+     <!-- 创建新用户对话框  通过visible来获取上面的点击绑定值-->
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%"
+    @close="addDialogClosed">
+      <!-- 内容主体区域 -->
+      <el-form :model="addForm" :rules="addFormRules"
+        ref="addFormRef" label-width="70px">
+        <!-- 用户名 -->
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <!-- 邮箱 -->
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 内容底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -81,42 +132,72 @@ export default {
       queryInfo: {
         query: "",
         pageNum: 1,
-        pageSize: 5
+        pageSize: 5,
       },
       userlist: [], // 用户列表
       total: 0, // 最大数据记录
+      addDialogVisible: false, // 对话框显示
+      // 添加用户单项
+      addForm: {
+        username: "",
+        password: "",
+        email: "",
+      },
+      //   控制修改用户对话框显示/隐藏
+      editDialogVisible: false,
+
+      //   验证规则
+      addFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 5, max: 8, message: "长度在5-8字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 8, message: "长度在 6 到 8 个字符", trigger: "blur" },
+        ],
+        email: [
+          { required: true, message: "请输入邮箱", trigger: "blur" },
+          { min: 5, max: 15, message: "请输入正确邮箱地址", trigger: "blur" },
+        ],
+      },
     };
   },
   // 各种的方法
   methods: {
     async getUserList() {
       // get方法访问后台的路径
-      const { data: res } = await this.$http.get("allUser", {params: this.queryInfo});
+      const { data: res } = await this.$http.get("allUser", {
+        params: this.queryInfo,
+      });
       this.userlist = res.data;
       this.total = res.numbers;
     },
     // 添加方法的使用
     // 现在的页码是多少
     handleSizeChange(newSize) {
-        this.queryInfo.pageSize = newSize;
-        this.getUserList();// 重新提交表单
+      this.queryInfo.pageSize = newSize;
+      this.getUserList(); // 重新提交表单
     },
     // 页码的跳转方法
     handleCurrentChange(newPage) {
-        this.queryInfo.pageNum = newPage;
-        this.getUserList();
-
+      this.queryInfo.pageNum = newPage;
+      this.getUserList();
     },
-    async userStateChanged(userInfo){
-        const {data: res} = await this.$http.put(`userState?id=${userInfo.id}&state=${userInfo.state}`);
-        if (res != "success") {
-            userInfo.state =! userInfo.state;
-            return this.$message.error("操作失败！");
-        }
-        this.$$message.success("操作成功！");
-
-    }
-
+    async userStateChanged(userInfo) {
+      const { data: res } = await this.$http.put(
+        `userState?id=${userInfo.id}&state=${userInfo.state}`
+      );
+      if (res != "success") {
+        userInfo.state = !userInfo.state;
+        return this.$message.error("操作失败！");
+      }
+      this.$$message.success("操作成功！");
+    },
+    // 重置表单
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields(); //重置表单
+    },
   },
 };
 </script>
